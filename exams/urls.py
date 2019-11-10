26bb1a15cfa07b97a71e18436e2af6ce9fcae51a
Oresko <oresko@gamma.ttk.pte.hu>
2019-11-10 23:29:05 +0100

    Példatár, Slug-Preferences Fix

--- exams/urls.py
+++ exams/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.searchlist, name='search_results'),
     path('preferences/', views.preferences, name='preferences'),
-    path('<slug:problem_slug>', views.task, name='task'),
+    path('blog/<slug:problem_slug>', views.task, name='task'),
 ]
